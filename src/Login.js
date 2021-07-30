aabe96454c408938e4bf4b6f2d99b5f5ce3b49ca
codingzilla <aj_chavez@sbcglobal.net>
2021-07-29 21:28:42 -0500

    LoginComponent

---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+//Try to make this a form. Use Art Collector as a skeleton to make this form.
+const Login = () => {
+  return (
+    <div>
+      <h3>Login form</h3>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/notes.js
@@ -0,0 +1,9 @@
+//First thing to do is create a form similar to the video. It doesn't need to be exactly the same thing either.
+
+//Components needed - Nav, Home, Posts, Login (possibly single posts as well)
+
+//All posts are coming from our API. Put that data into our state and render to the page
+
+//We don't need to have an edit button, it's too advanced for now.
+
+//For the form, we're using POST instead of GET
